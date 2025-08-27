1a0d8b61e229f0920ec0daac01ddbb33409fbf79
HYUNG BIN LEE <small0529@naver.com>
2025-08-27 17:50:29 +0900

    python for data engineering the second day

---
+++ Operations_in_Python/Operations_in_Python.py
@@ -0,0 +1,84 @@
+print("########################## Arithmetic Operator ############################")
+'''
+a = 10
+b = 2
+
+print(a + b) # 12
+print(a - b) # 8
+print(a * b) # 20
+print(a / b) # 5.0
+print(a // b) # 5
+print(a % b) # 0
+print(a ** b) # 제곱 100
+
+print("########################## Relational Operator ############################")
+
+a = 10
+b = 5
+
+print(a > b) # True
+print(a < b) # False
+print(a >= b) # True
+print(a <= b) # False
+
+
+print("########################## Eqality Operator ############################")
+a = 5
+b = 2
+print(a == b) # False
+print(a != b) # True
+
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+l3 = l1
+
+print(id(l1), id(l2), id(l3))
+print(id(l1) == id(l2)) # False
+print(id(l1) != id(l2)) # True
+print(l1 == l2) # False
+print(l1 != l2) # True
+print(l1 is l2) # False
+print(l1 is not l2) # True
+
+
+x = True
+y = False
+print(x and y) # False
+print(x or y) # True
+print(not x) # False
+print(not y) # True
+'''
+
+a=  True
+b = False
+c = True
+
+print(a & b) # False
+print(a & c) # True
+print(a | b) # True
+print(a | c) # True
+print(a ^ b) # True
+print(a ^ c) # False
+
+a = 10
+b = 20
+print(bin(a), bin(b))
+a1 = a << 2
+a2 = a >> 2
+print(bin(a1), bin(a2))
+
+c = 30 if a > b else 40
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
