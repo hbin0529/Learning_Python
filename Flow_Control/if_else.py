b5cda66324a9e0d9b87da5c1c3fd1562da3df67c
hbin0529 <small052@naver.com>
2025-08-28 23:43:47 +0900

    python for data engineering the third day

---
+++ Flow_Control/if_else.py
@@ -0,0 +1,12 @@
+print("Learning If Else selection statement")
+
+for i in range(5):
+    x = int(input("Enter Number "))
+    if x == 10:
+        print("your choice is 10")
+    elif x == 20:
+        print("your choice is 20")
+    elif x == 30:
+        print("your choice is 30")
+    else:
+        print("your choice number is not there")
